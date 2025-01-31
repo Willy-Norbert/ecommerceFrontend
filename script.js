7b710dd31cc682a4b944f28656d7827262fc1bbf
willy norbert <156060601+Willy-Norbert@users.noreply.github.com>
2025-01-31 11:22:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const product = {
+  name: "adults-plain-cotton-tshirt-2-pack-teal",
+  price: "$53.4",
+  count: " "
+}
+function addToCart() {
+
+  //get the cart container
+  const GetItemsContainer = 
+
+}
